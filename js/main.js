151fc99eaf2a63695bb1a46cd290c895dcc93648
Bonface Mwangi Wairimu <bwmwangi@safaricom.co.ke>
2023-03-13 09:49:27 +0300

    cleanup unecessary entries on JS

--- js/main.js
+++ js/main.js
@@ -2,22 +2,21 @@ const MALE_NAMES= ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
 const FEMALE_NAMES= ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 let userAkanName= null;
 let gender=null;
-//let validDate= null;
-//let validMonth= null;
+
 
 
 function getUserData() {
-let birthYear = prompt ("enter your year of birth");
+let birthYear = prompt ("Enter your year of birth: format xxyy eg 1980");
 let century = parseInt (birthYear.slice(0, 2));
 let year = parseInt (birthYear.slice(2, ));
-let month = parseInt( prompt("enter the month"));
-let date= parseInt(prompt("enter the date"));
-gender= prompt("enter the gender");
+let month = parseInt( prompt("Enter the month: between 1 to 12"));
+let date= parseInt(prompt("Enter the date : between 1 to 31"));
+gender= prompt("Enter the gender");
 let indexOfDay=Math.floor(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) %7);   
 return indexOfDay;
 
 }
-/*
+
 function checkValidMonth(){
     
     if (month<1 || month>12)
@@ -38,7 +37,7 @@ function checkValidDate(date){
    { validDate= true;
     return validDate
 }
-}*/
+}
 
 
 function getUserAkanName(){
@@ -50,32 +49,29 @@ let indexOfDay= getUserData();
             if (gender == "m" )
             {
                 userAkanName= MALE_NAMES[indexOfDay];
-                document.getElementById("output").innerHTML = "your Akan Name is : " + userAkanName; 
+                document.getElementById("output").innerHTML = "Your Akan Name is : " + userAkanName; 
                            
             } 
             else if (gender== "f" )
             {
                 userAkanName= FEMALE_NAMES[indexOfDay];
-                document.getElementById("output").innerHTML = "your Akan Name is : " + userAkanName;  
+                document.getElementById("output").innerHTML = "Your Akan Name is : " + userAkanName;  
                
             } 
         
             else 
             {
-            // console.log("Your Gender is unknown")
+            
             alert ("your data is invalid: confirm the date, the month or gender.");
-            //document.getElementById("output1").innerHTML = "your data is invalid: confirm the date, the month or gender.";  
-            }
+        
+        }
         
 }
 
 function main()
 {
-    
-//getUserData()
-//let indexOfDay= getUserData();
 getUserAkanName();
 }  
-//document.getElementById("output").innerHTML = "your Akan Name is : " + userAkanName;  
+
 
 
